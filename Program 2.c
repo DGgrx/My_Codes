a1197ed5f503befe4635b901c3b83a303cdf91aa
unknown <divyanshgarg.divs@gmail.com>
2020-12-23 15:35:58 +0530

    Initialising 2

--- Program 2.c
+++ Program 2.c
@@ -3,15 +3,15 @@ int main()
 {
     float x,y,z;
     float a,b,c;
-    printf("Enter the point as (X,Y,Z)");
-    printf("X=%.1f",x); scanf("%.1f",&x);
-    printf("Y=%.1f",y); scanf("%.1f",&y);
-    printf("Z=%.1f",z); scanf("%.1f",&z);
-    printf("The entered point is= (%.1f,%.1f,%.1f)", x,y,z);
-    printf("Enter the line as ax+by+c=0");
-    printf("a=%.1f",a); scanf("%.1f",&a);
-    printf("b=%.1f",b); scanf("%.1f",&b);
-    printf("c=%.1f",c); scanf("%.1f",&c);
+    printf("Enter the point as (X,Y,Z)\n");
+    printf("X=",x); scanf("%f",&x);
+    printf("Y=",y); scanf("%f",&y);
+    printf("Z=",z); scanf("%f",&z);
+    printf("The entered point is= (%.1f,%.1f,%.1f)\n\n", x,y,z);
+    printf("Enter the line as ax+by+c=0\n");
+    printf("a=",a); scanf("%f",&a);
+    printf("b=",b); scanf("%f",&b);
+    printf("c=",c); scanf("%f",&c);
     printf("The entered line is %.1fx+%.1fy+%.1f=0", a,b,c);
     return(0);
 }
