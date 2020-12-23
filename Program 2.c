3ddb13d73cc4fc4ba0ec380f0ab27d2dd5b42f5b
unknown <divyanshgarg.divs@gmail.com>
2020-12-23 15:54:33 +0530

    Program in 2D(corrected)

--- Program 2.c
+++ Program 2.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    float x,y,z;
+    float x,y;
     float a,b,c;
-    printf("Enter the point as (X,Y,Z)\n");
+    printf("Enter the point as (X,Y)\n");
     printf("X=",x); scanf("%f",&x);
     printf("Y=",y); scanf("%f",&y);
-    printf("Z=",z); scanf("%f",&z);
-    printf("The entered point is= (%.1f,%.1f,%.1f)\n\n", x,y,z);
+    printf("The entered point is= (%.1f,%.1f)\n\n", x,y);
     printf("Enter the line as ax+by+c=0\n");
     printf("a=",a); scanf("%f",&a);
     printf("b=",b); scanf("%f",&b);
     printf("c=",c); scanf("%f",&c);
-    printf("The entered line is %.1fx+%.1fy+%.1f=0", a,b,c);
-if(ax+by+cz==0)
-    printf("The point lies on the line.");
-    printf("the point does not lie on the line.");
+    printf("The entered line is %.1fx+%.1fy+%.1f=0\n\n", a,b,c);
+if(a*x+b*y+c==0)
+    { 
+       printf("The point lies on the line.");
+    }
+    else
+    {
+        printf("the point does not lie on the line.");
+    }
     return(0);
 }
