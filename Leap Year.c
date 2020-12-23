594e97b455aa7cae60103fde76645996ffe9b218
unknown <divyanshgarg.divs@gmail.com>
2020-12-23 16:35:05 +0530

    Renamed all

---
+++ Leap Year.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    double year;
+    printf("Enter the year : ");
+    scanf("%lf",&year);
+    if(year/4 == )
+    {
+        printf("The year %.0lf is a leap year", year);
+    }
+    else
+    {
+        printf("The year %.0lf is not a leap year", year);
+    }
+    return(0);
+}
