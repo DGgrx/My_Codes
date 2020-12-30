45ec1f97baf73af9fa27b6d83a9204e78e005607
unknown <divyanshgarg.divs@gmail.com>
2020-12-30 16:05:02 +0530

    prototype

--- Valid Triangle.c
+++ Valid Triangle.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int a, b, c, d;
-    scanf("%d %d %d", a, b, c);
-    printf("Enter the sides of the triangle");
-    if (a <= b && a <= c)
-    {
-        d = a;
-    }
-    else if (b <= a && b <= c)
-    {
-        d = b;
-    }
-    else
-    {
-        d = c;
-    }
-    if ((d <= a && d <= b) || (d <= c && d <= b) || (d <= a && d <= c))
-    {
-        printf("Valid");
-    }
-    else
-    {
-        printf("invalid");
-    }
-    return(0);
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+  if(a<(b+c) && b<(a+c) && c<(b+a))
+  {
+      printf("valid");
+  }
+  else
+  {
+      printf("invalid");
+  }
+return(0);
 }
