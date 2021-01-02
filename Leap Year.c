b6d0709c29fcf326018d06a684a05be489a73e49
unknown <divyanshgarg.divs@gmail.com>
2021-01-03 02:50:18 +0530

    Formatted

--- Leap Year.c
+++ Leap Year.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     double year;
     printf("Enter the year : ");
-    scanf("%lf",&year);
-    int q=year/4;
-    int r=year-(4*q);
-    if(r==0)
+    scanf("%lf", &year);
+    int q = year / 4;
+    int r = year - (4 * q);
+    if (r == 0)
     {
         printf("The year %.0lf is a leap year", year);
     }
@@ -14,5 +14,5 @@ int main()
     {
         printf("The year %.0lf is not a leap year", year);
     }
-    return(0);
+    return (0);
 }
